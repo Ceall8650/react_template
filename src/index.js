30fa17a7fdfbcbae1809d45f894a5979e165e90d
Ceall Shih <ceall8650@gmail.com>
2016-12-21 17:35:03 +0800

    以App.js為網頁主體

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        return ( 
-            <div><h1> Hello, World!223fdew2 </h1></div>
-        );
-    }
-}
 ReactDOM.render(<App />, document.getElementById('app'));
